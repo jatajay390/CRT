d8efddd4976571836509cf09238c2d7a295c8175
jatajay390 <jatajay390@gmail.com>
2026-05-06 15:09:02 +0530

    Add Day 1-6 C++ practice code

---
+++ Day-6/constructor.cpp
@@ -0,0 +1,12 @@
+#include<iostream>
+using namespace std;
+class students{
+    public:
+    string name;
+        int roll_no;
+        void show()
+        {
+            cout<<"name"<<name<<endl;
+            cout<<"roll number"<<roll_no<<endl;
+        }return 0;
+}
