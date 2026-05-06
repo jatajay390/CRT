6e930b5edd91c5a651916866d020e5149c196db1
jatajay390 <jatajay390@gmail.com>
2026-05-06 15:33:17 +0530

    initial commit

---
+++ Day-6/constructor_overloading.cpp
@@ -0,0 +1,36 @@
+#include <iostream>
+using namespace std;
+
+class students
+{
+public:
+    int roll_no;
+    string name;
+    
+    students()
+    {
+        roll_no = 0;
+        name = "Your Name Please";
+    }
+    
+
+    students(int r, string name)
+    {
+        roll_no = r;
+        this->name = name;
+    }
+    void show()
+    {
+        cout << "Name: " << name << endl;
+        cout << "Roll No: " << roll_no << endl;
+    }
+};
+
+int main()
+{
+    students s1;
+    s1.show();
+    students s2(18, "Ajay");
+    s2.show();
+    return 0;
+}
--- Day-7/test_auto_commit.cpp
+++
@@ -1,8 +0,0 @@
-// Test file for auto-commit system
-#include <iostream>
-using namespace std;
-
-int main() {
-    cout << "Auto-commit system test!" << endl;
-    return 0;
-}
